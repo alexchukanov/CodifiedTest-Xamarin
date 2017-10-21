5b5b0e34b8749c5df08945acad14e60e8972b944
Alexander Chukanov <alex.chukanov@gmail.com>
2017-10-21 21:30:07 +0300

    1st commit

---
+++ CodiTest/CodiTest/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CodiTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void OnLabelTapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
