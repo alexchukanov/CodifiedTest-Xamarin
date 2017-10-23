7dffd8d7d0b9e114851ee7d661b64c2c496e833c
Alexander Chukanov <alex.chukanov@gmail.com>
2017-10-23 16:56:41 +0300

    alfa no handlers

--- CodiTest/CodiTest/MainPage.xaml.cs
+++ CodiTest/CodiTest/MainPage.xaml.cs
@@ -9,11 +9,21 @@ namespace CodiTest
 {
     public partial class MainPage : ContentPage
     {
+       
+        public static readonly BindableProperty ImageNameTextProperty =
+                    BindableProperty.Create("ImageNamet", typeof(string), typeof(MainPage), "meR4.png");
+       
+
+        public string ImageName
+        {
+            set { SetValue(ImageNameTextProperty, value); }
+            get { return (string)GetValue(ImageNameTextProperty); }
+        }
+       
+       
         public MainPage()
         {
             InitializeComponent();
-
-            
         }
 
         private void OnLabelTapped(object sender, EventArgs e)
--- CodiTest/CodiTest/obj/Debug/CodiTest.MainPage.xaml.g.cs
+++ CodiTest/CodiTest/obj/Debug/CodiTest.MainPage.xaml.g.cs
@@ -15,16 +15,12 @@ namespace CodiTest {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
-        private global::Xamarin.Forms.StackLayout MainLayoute;
-        
-        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
-        private global::Xamarin.Forms.StackLayout ProfileLayoute;
+        private global::Xamarin.Forms.ContentView contentView;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
-            MainLayoute = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "MainLayoute");
-            ProfileLayoute = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "ProfileLayoute");
+            contentView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ContentView>(this, "contentView");
         }
     }
 }
